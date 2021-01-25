f9de0eb5cc77c12399241fa1acd6ba74c7dca815
Danish Thene <emergency0110123@gmail.com>
2021-01-25 10:28:47 +0700

    some code changes

--- app.js
+++ app.js
@@ -11,18 +11,30 @@ const dotenv = require("dotenv").config();
 // .config({path: path.join(__dirname, '.env')});or use this
 
  
- // INITIALISE THE EXPRESS APP
+ // INITIALISE THE EXPRESS APP AND EJS
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use("view engine", 'ejs');
+
 
 // IF THERE IS ANY STATIC FILE PUT IT IN PUBLIC FOLDER
 app.use(express.static("public"));
  
 //HOME ROUTE GET REQUESTS
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html");
+ 
+    let today = new Date();
+
+    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  
+    var day=days[today.getDay()];
+    
+  res.render('list', {kindofday: day});
+    
+    // variable day = array dari variable days dimana fungsi / method getday === hari dari 0-6
+    // see mdn doc or w3s tentang metod getday / new date() jadi saturday = 7 jika dimulai dari 1
 });
  
 //HOME ROUTE POST REQUESTS
