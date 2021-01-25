0137fd2b27086b5b1bb1871827c2127374af79d0
Danish Thene <emergency0110123@gmail.com>
2021-01-25 07:32:18 +0700

    adding ejs file to directory

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const dotenv = require ('dotenv').config;
 const bodyParser = require ('body-parser');
 const ejs = require ('ejs');
 
-// initiating express and body parser app
+// initiating express and body parser app and ejs
 
 const app = express();
 
-app.use(bodyParser(urlencoded{extended : true}));
+app.use("view engine", 'ejs');
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 // set home route app
 
 app.get('/', function (req , res) {
- res.sendFile(__dirname + 'index.html');
+ 
+let today = new Date();
+
+  var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  var day=days[today.getDay()];
+  
+res.render('list', {kindofday: day});
+  
+  // variable day = array dari variable days dimana fungsi / method getday === hari dari 0-6
+  // lihat documentasi mdn atau w3s tentang metod getday / new date() jadi saturday = 7 jika dimulai dari 1
+
+  
+
 
 });
 
